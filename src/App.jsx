37487eb8c4b7c50517e72300fc56902245989448
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-18 20:18:02 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ThemeManager from "./components/ThemeManager";
@@ -14,6 +15,7 @@ import ProductDetails from "./pages/ProductDetails";
 import ContactSeller from "./pages/ContactSeller";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
+
 import AddProduct from "./admin/AddProduct";
 import EditProduct from "./admin/EditProduct";
 import ProductsList from "./admin/ProductsList";
@@ -21,46 +23,58 @@ import ProductsList from "./admin/ProductsList";
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <div className="relative min-h-screen">
-          <ThemeManager />
-          <Navbar />
-          <main className="pt-20">
-            <Routes>
-              {/* Public Routes */}
-              <Route path="/" element={<Home />} />
-              <Route path="/categories" element={<Categories />} />
-              <Route path="/categories/:categoryId" element={<CategoryPage />} />
-              <Route path="/product/:id" element={<ProductDetails />} />
-              <Route path="/contact-seller" element={<ContactSeller />} />
-              <Route path="/login" element={<Login />} />
-              
-              {/* Protected Admin Routes */}
-              <Route path="/admin/dashboard" element={
+      <div className="relative min-h-screen">
+        <ThemeManager />
+        <Navbar />
+
+        <main className="pt-20">
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/categories/:categoryId" element={<CategoryPage />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/contact-seller" element={<ContactSeller />} />
+            <Route path="/login" element={<Login />} />
+
+            {/* Admin Only */}
+            <Route
+              path="/admin/dashboard"
+              element={
                 <ProtectedRoute>
                   <AdminDashboard />
                 </ProtectedRoute>
-              } />
-              <Route path="/admin/add-product" element={
+              }
+            />
+            <Route
+              path="/admin/add-product"
+              element={
                 <ProtectedRoute>
                   <AddProduct />
                 </ProtectedRoute>
-              } />
-              <Route path="/admin/edit/:id" element={
+              }
+            />
+            <Route
+              path="/admin/edit/:id"
+              element={
                 <ProtectedRoute>
                   <EditProduct />
                 </ProtectedRoute>
-              } />
-              <Route path="/admin/products" element={
+              }
+            />
+            <Route
+              path="/admin/products"
+              element={
                 <ProtectedRoute>
                   <ProductsList />
                 </ProtectedRoute>
-              } />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </BrowserRouter>
+              }
+            />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </AuthProvider>
   );
 }
